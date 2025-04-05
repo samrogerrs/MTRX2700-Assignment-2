3412dfb758136b36aca11ed7b288b8b6d1c455a8
pen-bro <pbro3848@uni.sydney.edu.au>
2025-04-06 09:13:45 +1000

    Added serial.c

---
+++ Exercise 2 - Serial Interface/src/serial.c
@@ -0,0 +1,219 @@
+#include "serial.h"
+
+#include "stm32f303xc.h"
+
+uint8_t rx_buffer[RX_BUFFER_SIZE];
+
+
+
+
+// We store the pointers to the GPIO and USART that are used
+//  for a specific serial port. To add another serial port
+//  you need to select the appropriate values.
+struct _SerialPort {
+	USART_TypeDef *UART;
+	GPIO_TypeDef *GPIO;
+	volatile uint32_t MaskAPB2ENR;	// mask to enable RCC APB2 bus registers
+	volatile uint32_t MaskAPB1ENR;	// mask to enable RCC APB1 bus registers
+	volatile uint32_t MaskAHBENR;	// mask to enable RCC AHB bus registers
+	volatile uint32_t SerialPinModeValue;
+	volatile uint32_t SerialPinSpeedValue;
+	volatile uint32_t SerialPinAlternatePinValueLow;
+	volatile uint32_t SerialPinAlternatePinValueHigh;
+	void (*completion_function)(uint32_t, char *);
+};
+
+
+
+
+
+// instantiate the serial port parameters
+//   note: the complexity is hidden in the c file
+SerialPort USART1_PORT = {USART1,
+		GPIOC,
+		RCC_APB2ENR_USART1EN, // bit to enable for APB2 bus
+		0x00,	// bit to enable for APB1 bus
+		RCC_AHBENR_GPIOCEN, // bit to enable for AHB bus
+		0xA00,
+		0xF00,
+		0x770000,  // for USART1 PC10 and 11, this is in the AFR low register
+		0x00, // no change to the high alternate function register
+		0x00 // default function pointer is NULL
+		};
+
+
+// InitialiseSerial - Initialise the serial port
+// Input: baudRate is from an enumerated set
+void SerialInitialise(uint32_t baudRate, SerialPort *serial_port, void (*completion_function)(uint32_t, char *)) {
+
+	serial_port->completion_function = completion_function;
+
+	// enable clock power, system configuration clock and GPIOC
+	// common to all UARTs
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	// enable the GPIO which is on the AHB bus
+	RCC->AHBENR |= serial_port->MaskAHBENR;
+
+	// set pin mode to alternate function for the specific GPIO pins
+	serial_port->GPIO->MODER = serial_port->SerialPinModeValue;
+
+	// enable high speed clock for specific GPIO pins
+	serial_port->GPIO->OSPEEDR = serial_port->SerialPinSpeedValue;
+
+	// set alternate function to enable USART to external pins
+	serial_port->GPIO->AFR[0] |= serial_port->SerialPinAlternatePinValueLow;
+	serial_port->GPIO->AFR[1] |= serial_port->SerialPinAlternatePinValueHigh;
+
+	// enable the device based on the bits defined in the serial port definition
+	RCC->APB1ENR |= serial_port->MaskAPB1ENR;
+	RCC->APB2ENR |= serial_port->MaskAPB2ENR;
+
+	// Get a pointer to the 16 bits of the BRR register that we want to change
+	uint16_t *baud_rate_config = (uint16_t*)&serial_port->UART->BRR; // only 16 bits used!
+
+	// Baud rate calculation from datasheet
+	switch(baudRate){
+	case BAUD_9600:
+		// NEED TO FIX THIS !
+		*baud_rate_config = 0x46;  // 115200 at 8MHz
+		break;
+	case BAUD_19200:
+		// NEED TO FIX THIS !
+		*baud_rate_config = 0x46;  // 115200 at 8MHz
+		break;
+	case BAUD_38400:
+		// NEED TO FIX THIS !
+		*baud_rate_config = 0x46;  // 115200 at 8MHz
+		break;
+	case BAUD_57600:
+		// NEED TO FIX THIS !
+		*baud_rate_config = 0x46;  // 115200 at 8MHz
+		break;
+	case BAUD_115200:
+		*baud_rate_config = 0x46;  // 115200 at 8MHz
+		break;
+	}
+
+
+	// enable serial port for tx and rx
+	serial_port->UART->CR1 |= USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+}
+
+void finished_transmission(uint32_t bytes_sent, char *sent_string) {
+	// This function will be called after a transmission is complete
+
+	volatile uint32_t test = 0;
+	// make a very simple delay
+	for (volatile uint32_t i = 0; i < 0x8ffff; i++) {
+		// waste time !
+	}
+}
+
+void finished_receiving(uint8_t num_characters, char *received_string){
+
+	volatile uint32_t test = 0;
+	for (volatile uint32_t i = 0; i < 0x8ffff; i++){
+		//delay
+	}
+}
+
+
+void SerialOutputChar(uint8_t data, SerialPort *serial_port) {
+
+	while((serial_port->UART->ISR & USART_ISR_TXE) == 0){
+	}
+
+	serial_port->UART->TDR = data;
+}
+
+void SerialOutputString(uint8_t *pt, SerialPort *serial_port) {
+
+	uint32_t counter = 0;
+	char start_string = pt;
+	while(*pt) {
+		SerialOutputChar(*pt, serial_port);
+		counter++;
+		pt++;
+	}
+
+	serial_port->completion_function(counter, (char *)start_string);
+}
+
+void USART1_EXTI25_IRQHandler(void){
+
+	/* (USART1->ISR & USART_ISR_ORE) {
+	    USART1->ICR |= USART_ICR_ORECF; // Clear overrun error flag
+	}
+	if (USART1->ISR & USART_ISR_NE) {
+	    USART1->ICR |= USART_ICR_NCF; // Clear noise error flag
+	}*/
+
+
+	if (USART1_PORT.UART->ISR & USART_ISR_RXNE){
+		static uint8_t rx_index = 0;
+		char received_byte = USART1_PORT.UART->RDR;
+
+		if(rx_index < RX_BUFFER_SIZE -1){
+			rx_buffer[rx_index] = received_byte;
+			rx_index++;
+		}
+
+		if(received_byte == '!'){
+			uint8_t length = rx_index;
+			rx_index = 0;
+			USART1_PORT.completion_function(length, rx_buffer);
+		}
+	}
+}
+
+
+void enable_USART_interrupt() {
+	// Disable the interrupts while messing around with the settings
+	//  otherwise can lead to strange behaviour
+	__disable_irq();
+
+	//enable the USART 1 clock
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+	//RXNE interrupt enable
+	USART1->CR1 |= USART_CR1_RXNEIE;
+	USART1->CR1 |= USART_CR1_RE | USART_CR1_TE; //Enable receiver + transmitter
+	USART1->CR1 |= USART_CR1_UE; //enable USART
+
+
+	// Tell the NVIC module that USART1 interrupts should be handled
+	NVIC_SetPriority(USART1_IRQn, 1);  // Set Priority
+	NVIC_EnableIRQ(USART1_IRQn);
+
+	// Re-enable all interrupts (now that we are finished)
+	__enable_irq();
+}
+
+
+void SerialInputChar(uint8_t *data, SerialPort *serial_port){
+
+	while((serial_port->UART->ISR & USART_ISR_RXNE) == 0){
+	}
+	*data = (uint8_t)(serial_port->UART->RDR);
+}
+
+void SerialInputString(uint8_t buffer_size, SerialPort *serial_port, char *buffer){
+
+	uint8_t buffer_counter = 0;
+	uint8_t character;
+	while (1){
+		SerialInputChar(&character, serial_port);
+		if (character == '!'){
+			break;
+		}
+		if (buffer_counter < buffer_size-1){
+			buffer[buffer_counter] = character;
+			buffer_counter++;
+		}
+	}
+	buffer[buffer_counter] = '!';
+	serial_port->completion_function(buffer_counter, buffer);
+}
+
