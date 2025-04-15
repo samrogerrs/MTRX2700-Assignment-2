90cef70b4f09b7471afefb287a948be5e35608b9
pen-bro <pbro3848@uni.sydney.edu.au>
2025-04-16 08:28:19 +1000

    Update integration.c

--- Ex 4 - not replacing incase ive broken sum/Src/integration.c
+++ Ex 4 - not replacing incase ive broken sum/Src/integration.c
@@ -13,22 +13,22 @@
 // and returns a string response to be sent over serial.
 const char* handle_command(const char *cmd, const char *args) {
 
-	if (strncmp(cmd, "serial", 7) == 0) {
+	if (strncmp(cmd, "serial ", 7) == 0) {
 		return args ? args : "";
 	}
-	else if (strncmp(cmd, "led", 4) == 0) {
+	else if (strncmp(cmd, "led ", 4) == 0) {
 		if (!args) return "Missing LED argument\r\n";
 		int mask = binary_to_int(args); // convert binary string to int
 		dio_set_led_state(mask); // update LED hardware
 		return "Finished task. Send new prompt";
 	}
-	else if (strncmp(cmd, "oneshot", 8) == 0) {
+	else if (strncmp(cmd, "oneshot ", 8) == 0) {
 	    if (!args) return "Missing delay argument\r\n";
 	    uint32_t delay = atoi(args); // convert delay string to int
 	    start_oneshot(delay, dio_invert_leds); // schedule LED inversion
 	    return "Finished task. Send new prompt";
 	}
-	else if (strncmp(cmd, "timer", 6) == 0) {
+	else if (strncmp(cmd, "timer ", 6) == 0) {
 	    if (!args) return "Missing period argument\r\n";
 	    uint32_t period = atoi(args); //convert period string to int
 	    dio_start_blinking();
